dd7bf84d8095efce8db1f9dab5f11307ed8d8877
danny allegrezza <danny@dannyallegrezza.com>
2017-10-05 13:11:09 -0400

    Implemented a basic queue
    Has typical functionality, including `Enqueue, Dequeue, Count, Clear`
    and enumeration functionality.

---
+++ DataStructures.Tests/QueueTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructures.Queues;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.Tests
+{
+    [TestClass]
+    public class QueueTests
+    {
+        [TestMethod]
+        public void TestEnqueueFunctionality()
+        {
+            var myQueue = new Queues.Queue<int>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                myQueue.Enqueue(i);
+            }
+
+            Assert.IsTrue(myQueue.Count > 0);
+            Assert.AreEqual(10, myQueue.Count);
+
+        }
+
+        [TestMethod]
+        public void TestDequeueFunctionality()
+        {
+            var myQueue = new Queues.Queue<int>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                myQueue.Enqueue(i);
+            }
+
+            Assert.IsTrue(myQueue.Count > 0);
+            Assert.AreEqual(10, myQueue.Count);
+
+            // Verify items are dequeued in the correct order
+            for (int i = 1; i <= 10; i++)
+            {
+                int item = myQueue.Dequeue();
+                Assert.AreEqual(i, item);
+            }
+        }
+    }
+}
---
+++ DataStructures/Queues/Queue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Queues
+{
+    public class Queue<T> : IEnumerable<T>
+    {
+        private LinkedList<T> _items = new LinkedList<T>();
+
+        /// <summary>
+        /// Adds an item to the queue
+        /// </summary>
+        /// <param name="value"></param>
+        public void Enqueue(T value)
+        {
+            _items.AddLast(value);
+        }
+
+        /// <summary>
+        /// Removes and returns the frontmost item from the queue
+        /// </summary>
+        /// <returns></returns>
+        public T Dequeue()
+        {
+            if (Count <= 0) throw new InvalidOperationException("No items to in the Queue to dequeue");
+
+            T value = _items.First.Value;
+            // now, remove from the queue
+            _items.RemoveFirst();
+            return value;
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        public int Count { get { return _items.Count; } }
+
+        public T Peek()
+        {
+            if (Count <= 0) throw new InvalidOperationException("No items to in the Queue to peek at");
+
+            return _items.First.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+    }
+}
