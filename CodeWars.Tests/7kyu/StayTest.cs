8f62c2c12ff49a2b65ccfd52fa368366c73e00b6
danny allegrezza <danny@dannyallegrezza.com>
2017-10-18 22:31:50 -0400

    CW: Find the stray number
    https://www.codewars.com/kata/find-the-stray-number/train/csharp
    
    First solution using LINQ.

---
+++ CodeWars.Tests/7kyu/StayTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeWars._7kyu;
+
+namespace CodeWars.Tests._7kyu
+{
+    [TestClass]
+    public class StayTest
+    {
+        [TestMethod]
+        public void SimpleArray1()
+        {
+            Assert.AreEqual(2, Stray.FindTheStray(new int[] { 1, 1, 2 }));
+        }
+    }
+}
---
+++ CodeWars/7kyu/Stray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeWars._7kyu
+{
+    public class Stray
+    {
+        public static int FindTheStray(int[] numbers)
+        {
+            var distinct =
+                from g in numbers.GroupBy(x => x)
+                where g.Count() == 1
+                select g.First();
+
+            var myVar = distinct.FirstOrDefault();
+            return myVar;
+        }
+    }
+}
