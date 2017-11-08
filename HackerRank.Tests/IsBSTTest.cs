cfd88b833ad6c54c31fc874d711484e1ab9c86ec
danny allegrezza <danny@dannyallegrezza.com>
2017-11-07 21:09:43 -0500

    Add "IsSuperBalanced" question and answer

--- HackerRank.Tests/IsBSTTest.cs
+++ HackerRank.Tests/IsBSTTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HackerRank.Tests
@@ -33,6 +34,71 @@ namespace HackerRank.Tests
             var result = IsThisABST.IsBST(bstree);
 
             Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void IsValidBSTPrintInOrder()
+        {
+            var bstree = new Node(10);
+            bstree.Insert(15);
+            bstree.Insert(5);
+            bstree.Insert(4);
+            bstree.Insert(9);
+
+            bstree.PrintInOrder();
+        }
+
+        [TestMethod]
+        public void FindSecondLargestElementInBinaryTreeIsValid()
+        {
+            var bstree = new Node(10);
+            bstree.Insert(15);
+            bstree.Insert(5);
+            bstree.Insert(4);
+            bstree.Insert(9);
+
+            var secondLargestNode = FindSecondLargest(bstree);
+
+            Assert.AreEqual(10, secondLargestNode);
+
+        }
+
+        private int FindSecondLargest(Node rootNode)
+        {
+            var current = rootNode;
+
+            while (true)
+            {
+                // Case: Current node is the largest and has a left subtree, so
+                // the 2nd largest node is the largest in *that* subtree
+                if (current.left != null && current.right == null)
+                {
+                    return FindLargest(current.left);
+                }
+
+                // Case: Current node is the parent of the largest node, and the largest has no children, so
+                // the current is the 2nd largest value
+                if (current.right != null
+                    && current.right.left == null
+                    && current.right.right == null)
+                {
+                    return current.data;
+                }
+                current = current.right;
+            }
+        }
+
+        private int FindLargest(Node rootNode)
+        {
+            var current = rootNode;
+
+            while (current.right != null)
+            {                
+                current = current.right;
+            }
+
+            return current.data;
+
         }
     }
 }
---
+++ IC.Tests/Trees/IsSuperBalancedTreeTests.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IC.Tests.Trees
+{
+    [TestClass]
+    public class IsSuperBalancedTreeTests
+    {
+        [TestMethod]
+        public void BinaryTreeIsNotSuperBalanced()
+        {
+            var root = new BinaryTreeNode(4);
+            root.InsertLeft(3);
+            root.InsertRight(5);
+            var leftNode = root.Left;
+            leftNode.InsertLeft(2);
+            var leftLeftNode = leftNode;
+            leftLeftNode.InsertLeft(1);
+            leftNode = leftLeftNode.Left;
+            leftNode.InsertLeft(2);
+
+            var expected = false;
+
+            var actual = BinaryTreeHelper.IsBalanced(root);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class BinaryTreeNode
+    {
+        public int Value { get; }
+
+        public BinaryTreeNode Left { get; private set; }
+
+        public BinaryTreeNode Right { get; private set; }
+
+        public BinaryTreeNode(int value)
+        {
+            Value = value;
+        }
+
+        public BinaryTreeNode InsertLeft(int leftValue)
+        {
+            Left = new BinaryTreeNode(leftValue);
+            return Left;
+        }
+
+        public BinaryTreeNode InsertRight(int rightValue)
+        {
+            Right = new BinaryTreeNode(rightValue);
+            return Right;
+        }
+    }
+
+    public class NodeDepthPair
+    {
+        public BinaryTreeNode Node {get;}
+        public int Depth { get; }
+
+        public NodeDepthPair(BinaryTreeNode node, int depth)
+        {
+            Node = node;
+            Depth = depth;
+        }
+    }
+    public class BinaryTreeHelper
+    {
+        public static bool IsBalanced(BinaryTreeNode treeRoot)
+        {
+
+            // a tree with no nodes is superbalanced, since there are no leaves!
+            if (treeRoot == null)
+            {
+                return true;
+            }
+
+            var depths = new List<int>(3); // We short-circuit as soon as we find more than 2
+
+            // Nodes will store pairs of a node and the node's depth
+            var nodes = new Stack<NodeDepthPair>();
+            nodes.Push(new NodeDepthPair(treeRoot, 0));
+
+            while (nodes.Count > 0)
+            {
+                // Pop a node and its depth from the top of our stack
+                var nodeDepthPair = nodes.Pop();
+                var node = nodeDepthPair.Node;
+                var depth = nodeDepthPair.Depth;
+
+                // Case: we found a leaf
+                if (node.Left == null && node.Right == null)
+                {
+                    // We only care if it's a new depth
+                    if (!depths.Contains(depth))
+                    {
+                        depths.Add(depth);
+
+                        // Two ways we might now have an unbalanced tree:
+                        //   1) more than 2 different leaf depths
+                        //   2) 2 leaf depths that are more than 1 apart
+                        if (depths.Count > 2
+                            || (depths.Count == 2 && Math.Abs(depths[0] - depths[1]) > 1))
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                // Case: this isn't a leaf - keep stepping down
+                else
+                {
+                    if (node.Left != null)
+                    {
+                        nodes.Push(new NodeDepthPair(node.Left, depth + 1));
+                    }
+
+                    if (node.Right != null)
+                    {
+                        nodes.Push(new NodeDepthPair(node.Right, depth + 1));
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
