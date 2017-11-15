ec190a0e7a68ed49a02997bc5fddfeee097cdb00
danny allegrezza <danny@dannyallegrezza.com>
2017-11-15 10:03:06 -0500

    Create logic and unit test for reversing a string in place in O(n) time O(1) space

---
+++ IC.Tests/Strings/ReverseStringInPlaceTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IC.Tests.Strings
+{
+    [TestClass]
+    public class ReverseStringInPlaceTests
+    {
+        [TestMethod]
+        public void TestReverseStringInPlaceIsValid()
+        {
+            string name = "danny";
+            name = name.ReverseStringInPlace();
+
+            Assert.AreEqual("ynnad", name);
+        }
+
+        [TestMethod]
+        public void TestReverseStringInPlaceIsValidWithOneCharacter()
+        {
+            string name = "d";
+            name = name.ReverseStringInPlace();
+
+            Assert.AreEqual("d", name);
+        }
+
+        [TestMethod]
+        public void TestReverseStringInPlaceHasInvalidInput()
+        {
+            string name = null;
+            name = name.ReverseStringInPlace();
+
+            Assert.AreEqual("", name);
+        }
+    }
+}
---
+++ IC.Tests/Strings/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IC.Tests.Strings
+{
+    public static class StringExtensions
+    {
+        public static string ReverseStringInPlace(this string s)
+        {
+            if (s == null) { return ""; }
+
+            char[] letters = s.ToCharArray();
+
+            for (int head = 0, tail = (letters.Length - 1); head < tail; head++, tail--)
+            {
+                char tmp = letters[head];
+                letters[head] = letters[tail];
+                letters[tail] = tmp;
+            }
+
+            return new string(letters);
+        }
+    }
+}
