b484e63e77841d77f15a2983df013294e78cdd8c
Danny Allegrezza <danny@dannyallegrezza.com>
2020-08-01 16:26:18 -0400

    Brute force attempt at "Strip Comments"

---
+++ CodeWars.Tests/4kyu/StripCommentsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars._4kyu;
+namespace CodeWars.Tests
+{
+    [TestClass]
+    public class StripCommentsTests
+    {
+        [TestMethod]
+        public void StripComments_WhenGivenEmptyString_ReturnsExpected()
+        {
+            var expected = "";
+            var actual = StripCommentsSolution.StripComments("", new string[] { "%", "$" });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StripComments_WhenGivenValidInput_ReturnsExpectedResult()
+        {
+            string stripped = StripCommentsSolution.StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new[] { "#", "!" });
+            string result = "apples, pears\ngrapes\nbananas";
+
+            Assert.AreEqual(stripped, result);
+        }
+    }
+}
---
+++ CodeWars/4kyu/StripComments.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace CodeWars._4kyu
+{
+    public class StripCommentsSolution
+    {
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return "";
+            }
+
+            var startingSymbol = char.Parse(commentSymbols[0]);
+            var endingSymbol = char.Parse(commentSymbols[1]);
+            var result = new StringBuilder();
+            var shouldRemoveTillEndOfLine = false;
+
+            using (StringReader reader = new StringReader(text))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var lineChars = line.ToCharArray();
+
+                    for (int index = 0; index < lineChars.Length; index++)
+                    {
+                        char currentChar = lineChars[index];
+
+                        if (currentChar == startingSymbol || currentChar == endingSymbol)
+                        {
+                            shouldRemoveTillEndOfLine = true;
+                            lineChars[index] = ' ';
+                        }
+
+                        if (shouldRemoveTillEndOfLine)
+                        {
+                            lineChars[index] = ' ';
+                        }
+                    }
+
+                    shouldRemoveTillEndOfLine = false;
+                    var strippedLine = new string(lineChars);
+
+                    result.Append(strippedLine.TrimEnd());
+                    result.Append("\n");
+                }
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
